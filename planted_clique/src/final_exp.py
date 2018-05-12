a6f3b5f5d65af30b08295eff6d66e1fe3628e54f
luca <luca.venturi.92@gmail.com>
2018-05-12 15:24:44 -0400

    final exp added

---
+++ planted_clique/src/final_exp.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn.functional as F
+import argparse
+
+import numpy as np
+import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from dataGenerator import dataGenerator
+from model import *
+from logger import *
+from trainer import train, test
+
+dtype = torch.cuda.FloatTensor
+dtype_l = torch.cuda.LongTensor
+
+logger = make_logger()
+
+clique_sizes = np.arange(5, 20, dtype=float) # 33
+test_results = {}
+for C in clique_sizes:
+    generator = dataGenerator()
+    generator.NUM_SAMPLES_train = 100# 10000
+    generator.N = 30# 200
+    J = 6
+    generator.J = J-2
+    num_features = 8 # must be even!
+    num_layers = 6
+    args = {'N' : generator.N, 'edge density' : 0.5, 'planted clique size' : C}
+    logger.args = args
+    generator.edge_density = args['edge density']
+    generator.clique_size = args['planted clique size']
+    generator.create_train_dataset()
+    print('Dataset created')
+    gnn = GNN(num_features, num_layers, J).type(dtype)
+    train(gnn, generator, logger, iterations=1000, batch_size=32) # 10000
+    
+    generator.NUM_SAMPLES_test = 100
+    generator.create_test_dataset()
+    print('Test dataset created')
+    test_results[cs, 'loss'], test_results[cs, 'accuracy'], _, test_results[cs, 'mismatch'] = test(gnn, generator, logger)
+    test_results[cs, 'mismatch'] /= C
+        
+# plot test loss
+
+plt.figure(0)
+plt.clf()
+plt.semilogy(densities, [test_results[cs, 'loss'] for cs in clique_sizes], 'b')
+plt.xlabel('Edge density')
+plt.ylabel('Cross Entropy Loss')
+plt.title('Test Loss: N={}'.format(logger.args['N']))
+plt.legend()
+path = 'plots/test_loss_N={}_trained_with_C'.format(logger.args['N'])
+plt.savefig(path)
+
+# plot test accuracy
+
+plt.figure(1)
+plt.clf()
+plt.plot(densities, [test_results[cs, 'accuracy'] for cs in clique_sizes], 'b')
+plt.xlabel('Edge density')
+plt.ylabel('Accuracy')
+plt.title('Test Accuracy: N={}'.format(logger.args['N']))
+plt.legend()
+path = 'plots/test_accuracy_N={}_trained_with_C'.format(logger.args['N'])
+plt.savefig(path)
+
+# plot test mismatch
+
+plt.figure(2)
+plt.clf()
+plt.plot(densities, [test_results[cs, 'mismatch'] for d in clique_sizes], 'b')
+plt.xlabel('Edge density')
+plt.ylabel('Mismatch')
+plt.title('Test Mismatch: N={}'.format(logger.args['N']))
+plt.legend()
+path = 'plots/test_mismatch_N={}_trained_with_C'.format(logger.args['N'])
+plt.savefig(path)
+
+# print
+
+for cs in clique_sizes:
+    print('CS = {} -> mismatch = {}').format(cs, test_results[cs, 'mismatch'])
